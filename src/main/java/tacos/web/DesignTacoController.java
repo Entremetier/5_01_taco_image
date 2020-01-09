d36d1d2fa55c8803106dd2b765ffd89a8c163d27
Marco Happe <marco.happe@icloud.com>
2020-01-09 19:40:20 +0100

    added: application.properties (turned caching off)

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -2,6 +2,7 @@ package tacos.web;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
@@ -22,22 +23,23 @@ import tacos.Taco;
 @Controller
 @RequestMapping("/design")
 public class DesignTacoController {
+
     @GetMapping
-    public String showDesignForm(Model model){
+    public String showDesignForm(Model model) {
         List<Ingredient> ingredients = Arrays.asList(
-        new Ingredient("FLTO","Flour Tortilla", Ingredient.Type.WRAP),
-        new Ingredient("COTO","CornTortilla", Ingredient.Type.WRAP),
-        new Ingredient("GRBF","Ground Beef", Ingredient.Type.PROTEIN),
-        new Ingredient("CARN","Carnitas", Ingredient.Type.PROTEIN),
-        new Ingredient("TMTO","Diced Tomatoes", Ingredient.Type.VEGGIES),
-        new Ingredient("LETC","Lettuce", Ingredient.Type.VEGGIES),
-        new Ingredient("CHED","Cheddar", Ingredient.Type.CHEESE),
-        new Ingredient("JACK","Monterrey Jack", Ingredient.Type.CHEESE),
-        new Ingredient("SLSA","Salsa", Ingredient.Type.SAUCE),
-        new Ingredient("SRCR","Sour Cream", Ingredient.Type.SAUCE)
+                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
         );
-        Ingredient.Type[] types =Ingredient.Type.values();
-        for (Ingredient.Type type : types){
+        Ingredient.Type[] types = Ingredient.Type.values();
+        for (Ingredient.Type type : types) {
             model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
         }
         model.addAttribute("design", new Taco());
@@ -45,20 +47,24 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(@Valid Taco design, Errors errors){
-        if (errors.hasErrors()){
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if (errors.hasErrors()) {
             return "design";
         }
         log.info("Verarbeitung design: " + design);
         return "redirect:/orders/current";
     }
 
-  private List<Ingredient> filterByType(
-      List<Ingredient> ingredients, Type type) {
-    return ingredients
-              .stream()
-              .filter(x -> x.getType().equals(type))
-              .collect(Collectors.toList());
-  }
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(new Predicate<Ingredient>() {
+                    @Override
+                    public boolean test(Ingredient x) {
+                        return x.getType().equals(type);
+                    }
+                })
+                .collect(Collectors.toList());
+    }
 }
 
